841dac7eb64bd1179c2b8e7785983485d4bcff29
Jayashree <jayashree.kammu@gmail.com>
2019-06-17 21:10:00 +0530

    electronicshop problem

---
+++ src/com/company/ElectronicShop.java
@@ -0,0 +1,60 @@
+//https://www.hackerrank.com/challenges/electronics-shop/problem
+package com.company;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+class SelectItemsForBuy {
+    int selectItemForBuy(int budget, List<Integer> keyboards, List<Integer> drives) {
+        int result = -1;
+        for (Integer integer : keyboards) {
+            for (Integer value : drives) {
+                if (integer + value <= budget && integer + value > result) {
+                    result = integer + value;
+
+                }
+            }
+        }
+        return result;
+    }
+}
+
+public class ElectronicShop {
+    public static void main(String[] args) {
+        ElectronicShop electronicShop=new ElectronicShop();
+        SelectItemsForBuy selectItemsForBuy = new SelectItemsForBuy();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the budget, number of items in the first list and second list :");
+        String[] input = scanner.nextLine().split(" ");
+        List<String> arrayList = new ArrayList<>(Arrays.asList(input));
+        int budget = Integer.parseInt(arrayList.get(0));
+        int keyboards = Integer.parseInt(arrayList.get(1));
+        int drives = Integer.parseInt(arrayList.get(2));
+        List<Integer> firstList = electronicShop.getFirstList(scanner, keyboards);
+        List<Integer> secondList = electronicShop.getSecondList(scanner, drives);
+        int maxValue = selectItemsForBuy.selectItemForBuy(budget, firstList, secondList);
+        System.out.println("Final budget cost: " + maxValue);
+    }
+
+     List<Integer> getSecondList(Scanner scanner, int drives) {
+        System.out.println("Enter the items in the secondList");
+        List<Integer> secondList = new ArrayList<>();
+        for (int j = 0; j < drives; j++) {
+            String secondArray = scanner.nextLine();
+            secondList.add(Integer.parseInt(secondArray));
+        }
+        return secondList;
+    }
+
+     List<Integer> getFirstList(Scanner scanner, int keyboards) {
+        System.out.println("Enter the items in the firstList");
+        List<Integer> firstList = new ArrayList<>();
+        for (int i = 0; i < keyboards; i++) {
+            String firstArray = scanner.nextLine();
+            firstList.add(Integer.parseInt(firstArray));
+        }
+        return firstList;
+    }
+}
+
